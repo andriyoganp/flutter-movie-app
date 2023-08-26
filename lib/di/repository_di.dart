46e7a86908827e8f414bb6560b2387b6257c851d
Andri Yoga Nur Pratama <andri.pratam@mapan.id>
2023-08-26 17:38:46 +0700

    feat(di): add di for repository

---
+++ lib/di/repository_di.dart
@@ -0,0 +1,11 @@
+import 'package:get_it/get_it.dart';
+
+import '../data/repository/movie_repository.dart';
+
+void initRepository(GetIt getIt) {
+  getIt.registerLazySingleton<MovieRepository>(
+    () => MovieRepositoryImpl(
+      getIt(),
+    ),
+  );
+}
