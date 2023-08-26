b8051896370982de1a0228cc298adccfc83219bc
Andri Yoga Nur Pratama <andri.pratam@mapan.id>
2023-08-26 17:41:20 +0700

    feat: setup entry point main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import '../di/injection.dart';
 import 'moviedb_app.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
+  initInjection();
+  runApp(const MyApp());
+}
