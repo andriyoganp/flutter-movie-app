e10022fd5c275b5ea68115451b7983edfd9c4ba7
Andri Yoga Nur Pratama <andri.pratam@mapan.id>
2023-08-28 16:51:04 +0700

    feat(core): add use case for stream

--- lib/core/interactor/use_case.dart
+++ lib/core/interactor/use_case.dart
@@ -6,4 +6,8 @@ abstract class UseCase<Type, Params> {
   Future<Either<Failure, Type>> call(Params params);
 }
 
+abstract class UseCaseStream<Type, Params> {
+  Stream<Type> call(Params params);
+}
+
 class NoParams {}
