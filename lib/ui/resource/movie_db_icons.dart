a78e527c382f80585406b8b0633fc3f532ab2a3e
Andri Yoga Nur Pratama <andri.pratam@mapan.id>
2023-08-28 16:52:57 +0700

    chore(ui): unused variable

--- lib/ui/resource/movie_db_icons.dart
+++ lib/ui/resource/movie_db_icons.dart
@@ -4,10 +4,9 @@ class MovieDBIcons {
   MovieDBIcons._();
 
   static const String _kFontFam = 'MovieDBIcons';
-  static const String? _kFontPkg = null;
 
-  static const IconData favorite = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData marker = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData popular = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData home = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData favorite = IconData(0xe800, fontFamily: _kFontFam);
+  static const IconData marker = IconData(0xe801, fontFamily: _kFontFam);
+  static const IconData popular = IconData(0xe802, fontFamily: _kFontFam);
+  static const IconData home = IconData(0xe803, fontFamily: _kFontFam);
 }
