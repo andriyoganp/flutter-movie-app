af080997d988219f66031be2b80be9bf34aece2e
Andri Yoga Nur Pratama <andri.pratam@mapan.id>
2023-08-28 16:52:24 +0700

    feat(presentation): set color from seed with primary

--- lib/moviedb_app.dart
+++ lib/moviedb_app.dart
@@ -6,6 +6,7 @@ import 'domain/use_case/get_movie_list_use_case.dart';
 import 'presentation/cubit/movie_list_cubit.dart';
 import 'presentation/page/main_page.dart';
 import 'router/router_list.dart';
+import 'ui/resource/ui_colors.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -18,23 +19,9 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         routes: RouterList.list,
         initialRoute: RouterList.initialRoute,
-        title: 'Flutter Demo',
+        title: 'MovieDB',
         theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // TRY THIS: Try running your application with "flutter run". You'll see
-          // the application has a blue toolbar. Then, without quitting the app,
-          // try changing the seedColor in the colorScheme below to Colors.green
-          // and then invoke "hot reload" (save your changes or press the "hot
-          // reload" button in a Flutter-supported IDE, or press "r" if you used
-          // the command line to start the app).
-          //
-          // Notice that the counter didn't reset back to zero; the application
-          // state is not lost during the reload. To reset the state, use hot
-          // restart instead.
-          //
-          // This works for code too, not just values: Most code changes can be
-          // tested with just a hot reload.
+          colorScheme: ColorScheme.fromSeed(seedColor: UiColors.primary),
         ),
         home: const MainPage(),
       ),
